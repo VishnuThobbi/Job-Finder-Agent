ad4278d0abfec656208236be5594c08968203700
Vishnu Thobbi <thobbi49@gmail.com>
2026-02-20 23:07:49 +0530

    added docker file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from langchain_core.documents import Document
 
 import pdfplumber
 import streamlit as st
+import requests
 import pdfplumber as pdf
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.llms.ollama import Ollama
@@ -13,7 +14,20 @@ import json
 
 from sqlalchemy.testing.suite.test_reflection import metadata
 
-from main import fetch_jobs
+
+def fetch_jobs(keyword):
+    url =  "https://jsearch.p.rapidapi.com/search"
+    headers = {"x-rapidapi-key": "7facd78fdfmsh4b407824e737441p188d1ejsn4406fecf56f9"}
+
+    all_jobs = []
+    # for page in range(1, 11):
+    #     response = requests.get( url, headers=headers, params={"query": keyword, "page": 1}).json()
+    #     all_jobs += response["data"]
+
+    print(all_jobs)
+
+    #return all_jobs
+    return requests.get( url, headers=headers, params={"query": keyword, "page": 1}).json()
 
 st.markdown("""
 <style>
